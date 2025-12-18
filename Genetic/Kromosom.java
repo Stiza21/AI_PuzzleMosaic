b9967f1a627d68a3758b952acbd8abf3765ece8a
Coretryxx <cleon.adam88@gmail.com>
2025-12-18 20:37:57 +0700

    fix code kromosom

--- Genetic/Kromosom.java
+++ Genetic/Kromosom.java
@@ -1,11 +1,48 @@
 package Genetic;
 
-public class Kromosom implements Comparable<Kromosom>{
-    private Gene [] arrOfKromosom;
+//Kromosom -> satu konfigurasi puzzle mosaic 
+//Gene -> satu sel mosaic  warna / tile ID
+
+public class Kromosom implements Comparable<Kromosom> {
+    private int[] genes;
+
+    public Kromosom(int[] genes) {
+        this.genes = genes;
+    }
+
+    public Kromosom(int PanjangKromosom) {
+        this.genes = new int[PanjangKromosom];
+    }
+
+    public int getGene(int idx) {
+        return genes[idx];
+    }
+
+    public void setGene(int index, int value) {
+        if (value != 0 && value != 1) {
+            throw new IllegalArgumentException(
+                "Nilai gen harus 0 (putih) atau 1 (hitam)"
+            );
+        }
+        genes[index] = value;
+    }
+
+    public int length() {
+        return genes.length;
+    }
+
+    public int[] getGenes() {
+        return genes.clone();
+    }
+
+    public Kromosom copy() {
+        return new Kromosom(genes);
+    }
+
     @Override
     public int compareTo(Kromosom o) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
     }
-    
+
 }
