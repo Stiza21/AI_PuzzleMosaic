a2c03449e334f496fea5cf419f963422bffb3ed3
Stiza21 <Fadjarmaulanasidiq575@gmail.com>
2025-12-19 20:04:46 +0700

    Test

--- Genetic/KotakHitam.java
+++ Genetic/KotakHitam.java
@@ -1,7 +1,10 @@
-package Genetic;
 
 public class KotakHitam {
     private int sbX;
     private int sbY;
     
+    public KotakHitam(int sbX, int sbY) {
+        this.sbX = sbX;
+        this.sbY = sbY;
+    }
 }
