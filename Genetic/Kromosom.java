6ea813bfee223eb245c7c78cdeb4d126847307c7
Akira-Kazu <241756796+Akira-Kazu@users.noreply.github.com>
2026-01-16 13:28:40 +0700

    penambahan layout dan fitness

--- Genetic/Kromosom.java
+++ Genetic/Kromosom.java
@@ -64,4 +64,21 @@ public class Kromosom implements Comparable<Kromosom> {
     public double konversiFitness(){ //isi sesudah ada fitness
         return 0.0;
     }
+
+    public int fitness(Layout layout){
+        int total=0;
+        for (int i=0;i<layout.request.length;i++){
+            for (int j=0;j<layout.request[i].length;j++){
+                int deviation =layout.request[i][j];
+                for (int k=i-1;k<=i+1;k++){
+                    for (int l=j-1;l<=j+1;l++){
+                        if (k>0&&k<layout.request.length&&l>0&&l<layout.request[i].length) deviation-=this.genes[k*layout.request[i].length+l];
+                    }
+                }
+                total+=Math.abs(deviation);
+            }
+        }
+        return total;
+    }
+
 }
